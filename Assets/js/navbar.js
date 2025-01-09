c4645ddcb7e1a4a9e4ea3e99c82832d78ef7d9bb
syedhamza6448 <164435480+syedhamza6448@users.noreply.github.com>
2025-01-09 23:28:49 +0500

    Fixed Navbar

--- Assets/js/navbar.js
+++ Assets/js/navbar.js
@@ -1,15 +1,17 @@
 let menuList = document.getElementById("menuList");
 menuList.style.maxHeight = "0px";
+let logoImg = document.getElementById("logo");
+
 
 function toggleMenu() {
-    let menuIcon = document.getElementById("menuIcon").children[0];
+    let menuIcon = document.getElementById("menuIcon")  ;
     if (menuList.style.maxHeight == "0px") {
         menuList.style.maxHeight = "400px";
         menuIcon.classList.remove("fa-bars");
-        menuIcon.classList.add("fa-times");
+        menuIcon.classList.add("fa-xmark");
     } else {
         menuList.style.maxHeight = "0px";
-        menuIcon.classList.remove("fa-times");
+        menuIcon.classList.remove("fa-xmark");
         menuIcon.classList.add("fa-bars");
     }
 }
@@ -17,12 +19,22 @@ function toggleMenu() {
 
 window.addEventListener('scroll', function() {
     let nav = document.querySelector('nav');
-    nav.style.transition = 'height 0.3s';
+    nav.style.transition = 'all 0.3s';
+    menuList.style.transition = 'all 0.3s';
+    logoImg.style.transition = 'all 0.3s';
     if (window.scrollY > 10) {
-        nav.style.height = '70px';
-        nav.style.backgroundColor = "#1d1d1fd7"
+        nav.style.height = '50px';
+        nav.style.backgroundColor = "#1d1d1fd7";
+        nav.style.borderBottom = 'none';
+        menuList.style.top = "50px";
+        menuList.style.backgroundColor = "#1d1d1fd7";
+        logoImg.style.height = "40px";
     } else {
         nav.style.height = '80px';
-        nav.style.backgroundColor = "#1d1d1f"
+        nav.style.backgroundColor = "#1d1d1f";
+        nav.style.borderBottom = '1px solid #f0cf59';
+        menuList.style.top = "80px";
+        menuList.style.backgroundColor = "#1d1d1f";
+        logoImg.style.height = "60px";
     }
 });
